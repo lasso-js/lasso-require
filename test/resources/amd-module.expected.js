18d1352de872c5c41daa835de1a1251dc42707af
Patrick Steele-Idem <pnidem@gmail.com>
2014-03-26 15:08:03 -0600

    Fixed test cases

--- test/resources/amd-module.expected.js
+++ test/resources/amd-module.expected.js
@@ -1,5 +1,5 @@
-$rmod.def("/foo@1.0.0/lib/index", function(require, exports, module, __filename, __dirname) { exports.foo = '1.0.0';
-var target = 'baz';
+$rmod.def("/foo@1.0.0/lib/index", function(require, exports, module, __filename, __dirname) { exports.foo = "1.0.0";
+var target = "baz";
 require(target); });
 $rmod.dep("", "foo", "1.0.0");
 $rmod.main("/foo@1.0.0", "lib/index");
